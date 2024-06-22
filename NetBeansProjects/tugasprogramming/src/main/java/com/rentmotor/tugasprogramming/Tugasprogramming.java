6bd19e3b572ca22711d130c64c329782058353cf
errygautama <errygautama@gmail.com>
2024-06-22 23:52:57 +0700

    first first

--- NetBeansProjects/tugasprogramming/src/main/java/com/rentmotor/tugasprogramming/Tugasprogramming.java
+++ NetBeansProjects/tugasprogramming/src/main/java/com/rentmotor/tugasprogramming/Tugasprogramming.java
@@ -12,5 +12,10 @@ public class Tugasprogramming {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
